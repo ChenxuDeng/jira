44103086b60f3ccafbb8c04fa4ebb7c8d48e172c
jimmy <836081346@qq.com>
2021-08-10 00:22:50 +0800

    added login&register

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import './App.css';
 import ProjectList from "./pages/projectList";
 import {TsReactTest} from "./pages/tryUseArray";
 import Login from "./pages/login/login";
+import {useAuth} from "./context/authContext";
+import {Authentivated} from "./authentivated";
+import {Unauthenticated} from "./unauthenticated";
 
 function App() {
+    const {user}=useAuth()
   return (
     <div className="App">
         {/*<ProjectList/>*/}
         {/*<TsReactTest/>*/}
-        <Login/>
+        {user?<Authentivated/>:<Unauthenticated/>}
     </div>
   );
 }
